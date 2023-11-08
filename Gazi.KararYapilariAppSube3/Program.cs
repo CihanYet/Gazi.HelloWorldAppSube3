6c024beaf9eb1eed9cf73abbbb1a6f4632de7b1f
Gazi <Gazi@DESKTOP-224SE1B>
2023-11-08 11:56:31 +0300

    Karar Yapıları-Try-Catch

--- Gazi.KararYapilariAppSube3/Program.cs
+++ Gazi.KararYapilariAppSube3/Program.cs
@@ -13,35 +13,57 @@
             //    Console.WriteLine("False değer");
             //}
 
-            Console.WriteLine("1.sayıyı giriniz:");
-            double s1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("2. sayıyı giriniz:");
-            double s2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("İşlem seçiniz:+,-,/,*");
-            char islem = char.Parse(Console.ReadLine());
-            string msg = "İşlemin Sonucu:";
-            if (islem == '+')
-            {
-                Console.WriteLine($"{msg}{s1 + s2}");
-            }
-            else if (islem == '-')
-            {
-                Console.WriteLine($"{msg}{s1 - s2}");
-            }
-            else if (islem == '*')
-            {
-                Console.WriteLine($"{msg}{s1 * s2}");
-            }
-            else if (islem == '/')
-            {
-                Console.WriteLine($"{msg}{s1 / s2}");
-            }
-            else
-            {
-                Console.WriteLine("Hatalı işlem seçtiniz!");
-            }
 
 
+            //Console.WriteLine("Dil seçiniz/Select Language(TR/EN):");
+            //string cevap = Console.ReadLine().ToUpper();
+            //string msg1, msg2, msg3, msg4, msg5;
+
+            //if (cevap=="TR")
+            //{
+            //    msg1 = "1.Sayıyı giriniz:";
+            //    msg2 = "2.Sayıyı giriniz:";
+            //    msg3 = "İşlem seçiniz:";
+            //    msg4 = "İşlemin sonucu:";
+            //    msg5 = "Hatalı işlem seçtiniz!";
+            //}
+            //else 
+            //{
+            //    msg1 = "Enter first number:";
+            //    msg2 = "Enter second number:";
+            //    msg3 = "Select Operation:";
+            //    msg4 = "Result:";
+            //    msg5 = "Incorrect Operation Selected!";
+            //}
+
+
+            //Console.WriteLine(msg1);
+            //double s1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine(msg2);
+            //double s2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine(msg3);
+            //char islem = char.Parse(Console.ReadLine());
+
+            //if (islem == '+')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 + s2}");
+            //}
+            //else if (islem == '-')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 - s2}");
+            //}
+            //else if (islem == '*')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 * s2}");
+            //}
+            //else if (islem == '/')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 / s2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(msg5);
+            //}
 
 
 
@@ -57,9 +79,60 @@
             //{
             //    Console.WriteLine("Sayılar eşit");
             //}
+
+            Console.WriteLine("Hava nasıl?");
+            string durum = Console.ReadLine().ToLower().Trim();
+            if (durum == "güzel" || durum == "iyi")
+            {
+                Console.WriteLine("Kaç derece?");
+                sbyte derece = sbyte.Parse(Console.ReadLine());
+                if (derece <= 15)
+                {
+                    Console.WriteLine("Soğuk evde otur");
+                }
+                else if (derece > 15 && derece <= 35)
+                {
+                    Console.WriteLine("Dışarı çık");
+                }
+                else
+                {
+                    Console.WriteLine("Çok sıcak evde otur");
+                }
+            }
+            else if (durum == "kötü")
+            {
+                Console.WriteLine("Evde otur");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş!");
+            }
+
+
+
+            //try
+            //{
+            //    Console.WriteLine("Sayı giriniz:");
+            //    byte sayi = byte.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Rakamla giriniz");
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("0-255 arası değer giriniz");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Bilinmeyen hata!");
+            //}
         }
     }
 }
+
+//Exception Handling-try-catch
 
 
 
